c73ec5edd413358c84b51c792efbf55b3710c2d9
Mohmmed-Mahsoub <mohamedmahsoub181@gmail.com>
2023-08-19 07:45:25 +0300

    feat: show courses in all courses page and allow user to navigate to add course, show course and edit course pages

--- src/pages/showAllCourses.page.js
+++ src/pages/showAllCourses.page.js
@@ -1,14 +1,33 @@
+import { Link } from "react-router-dom";
+import { AiOutlineAppstoreAdd } from "react-icons/ai";
+import { Button, Container } from "react-bootstrap";
+import CoursesCardItem from "../components/courses/coursesCardItem/coursesCardItem.component";
+
 const ShowAllCourses = () => {
   return (
     <section
       style={{
-        height: "100vh",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
+        minHeight: "100vh",
       }}
     >
-      <h1>ShowAllCourses</h1>
+      <div className="topBar py-3">
+        <Container className="d-flex justify-content-between align-items-center">
+          <h2 className="fs-3">All Courses</h2>
+          <Link to="/addCourse">
+            <Button>
+              <span>Add Course</span>
+              <i className="ms-2 fs-6 fs-sm-5">
+                <AiOutlineAppstoreAdd />
+              </i>
+            </Button>
+          </Link>
+        </Container>
+      </div>
+      <Container>
+        <div className="row">
+          <CoursesCardItem />
+        </div>
+      </Container>
     </section>
   );
 };
