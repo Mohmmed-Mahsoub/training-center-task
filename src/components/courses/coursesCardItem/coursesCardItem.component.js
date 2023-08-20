8fda97c77ebc7f2327dae77f3abb352aa4d57885
Mohmmed-Mahsoub <mohamedmahsoub181@gmail.com>
2023-08-20 22:11:33 +0300

    feat: show all courses UI and functionality

--- src/components/courses/coursesCardItem/coursesCardItem.component.js
+++ src/components/courses/coursesCardItem/coursesCardItem.component.js
@@ -1,21 +1,58 @@
-import { Button } from "react-bootstrap";
+import { useState } from "react";
+import { Button, Spinner } from "react-bootstrap";
 import styles from "./CoursesCardItem.module.css";
 import { Link } from "react-router-dom";
 import { FaTrashAlt } from "react-icons/fa";
 import { FiEdit } from "react-icons/fi";
+import axios from "axios";
+import { showToast } from "../../../helpers/showToast";
 
-const CoursesCardItem = () => {
+const CoursesCardItem = ({
+  coursesData: { course_name, id },
+  setCourses,
+  courses,
+}) => {
+  const [loadingDel, setLoadingDel] = useState(false);
+
+  const handleDelCourse = async (event, id) => {
+    event.preventDefault(); //stop routiog only when press del btn
+    setLoadingDel(true);
+
+    //delet course
+    try {
+      const res = await axios.delete(
+        `https://sheetdb.io/api/v1/owv6j5nrau720//id/${id}`
+      );
+      if (res?.data?.deleted) {
+        setLoadingDel(false);
+        //immediatly delete course to avoid need for reload page
+        const restCoursesAfterDel = courses.filter(
+          (course) => course.id !== id
+        );
+        setCourses(restCoursesAfterDel);
+        //show success after delete
+        showToast({
+          type: "success",
+          messgae: "success to delete couese",
+        });
+      }
+    } catch (error) {
+      setLoadingDel(false);
+      console.log(error);
+    }
+  };
+
   return (
     <div className="col-12 col-md-6 col-lg-4 col-xl-3 mt-4">
-      <Link to="/showCourse/20">
+      <Link to={`/showCourse/${id}`}>
         <div className={styles.cardContContainer}>
           <h3 className="fw-bold fs-5 text-center position-relative">
-            Course Name
+            {course_name}
           </h3>
           <div
             className={`${styles.categoryControlsContainer} d-flex justify-content-center position-relative`}
           >
-            <Link to="/editCourse/20">
+            <Link to={`/editCourse/${id}`}>
               <Button
                 className={`d-flex align-items-center justify-content-center ${styles.adjust} ${styles.categoryControl}`}
               >
@@ -24,10 +61,24 @@ const CoursesCardItem = () => {
                 </i>
               </Button>
             </Link>
-            <Button className={`${styles.delete} ${styles.categoryControl}`}>
-              <i className="d-flex justify-content-center">
-                <FaTrashAlt />{" "}
-              </i>
+            <Button
+              onClick={(event) => handleDelCourse(event, id)}
+              className={`${styles.delete} ${styles.categoryControl}`}
+            >
+              {loadingDel ? (
+                <Spinner
+                  as="span"
+                  animation="border"
+                  size="sm"
+                  role="status"
+                  aria-hidden="true"
+                  className={styles.loading}
+                />
+              ) : (
+                <i className="d-flex justify-content-center">
+                  <FaTrashAlt />{" "}
+                </i>
+              )}
             </Button>
           </div>
         </div>
--- src/pages/showAllCourses.page.js
+++ src/pages/showAllCourses.page.js
@@ -2,8 +2,30 @@ import { Link } from "react-router-dom";
 import { AiOutlineAppstoreAdd } from "react-icons/ai";
 import { Button, Container } from "react-bootstrap";
 import CoursesCardItem from "../components/courses/coursesCardItem/coursesCardItem.component";
+import MainLoader from "../components/general/mainLoader/mainLoader.component";
+import NoItemPlaceholder from "../components/general/noItemPlaceholder/noItemPlaceholder.component";
+import useSWR from "swr";
+import { useEffect, useState } from "react";
 
 const ShowAllCourses = () => {
+  //fetch all courses by SWR to easy handle fetching and caching data if there isn't any changes to avoid unnessary requests
+  const fetcher = (url) => fetch(url).then((res) => res.json());
+  const {
+    data: fetchedCourses,
+    error,
+    isLoading,
+  } = useSWR("https://sheetdb.io/api/v1/owv6j5nrau720", fetcher);
+
+  // State variable to store the processed data to easly change UI by change the state when delete course
+  const [courses, setCourses] = useState([]);
+
+  // Update the courses state after data is fetched
+  useEffect(() => {
+    if (fetchedCourses) {
+      setCourses(fetchedCourses);
+    }
+  }, [fetchedCourses]);
+
   return (
     <section
       style={{
@@ -24,9 +46,26 @@ const ShowAllCourses = () => {
         </Container>
       </div>
       <Container>
-        <div className="row">
-          <CoursesCardItem />
-        </div>
+        {isLoading ? (
+          <MainLoader />
+        ) : error ? (
+          <div> An error has occurred.</div>
+        ) : courses?.length > 0 ? (
+          <div className="row">
+            {courses?.map((coursesData) => {
+              return (
+                <CoursesCardItem
+                  coursesData={coursesData}
+                  key={coursesData.id}
+                  setCourses={setCourses}
+                  courses={courses}
+                />
+              );
+            })}
+          </div>
+        ) : (
+          <NoItemPlaceholder text="No Courses added yet" size="fs-5" />
+        )}
       </Container>
     </section>
   );
