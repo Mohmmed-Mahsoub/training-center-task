a8549c338c4decb778fb2b5d46f7f8645f7b857b
Mohmmed-Mahsoub <mohamedmahsoub181@gmail.com>
2023-08-20 22:13:27 +0300

    feat: show course UI and functionality

---
+++ src/components/courses/showCourse/showCourse.component.js
@@ -0,0 +1,36 @@
+import { Container, Table } from "react-bootstrap";
+import styles from "./ShowCourse.module.css";
+
+const ShowCourseComp = ({
+  courseData: { id, course_name, capacity, status, start_date, end_date },
+}) => {
+  return (
+    <Container className="mt-4">
+      <h1>{course_name}</h1>
+      <div className="row">
+        <div className="col-6">
+          <div className="d-flex p-3">
+            <span className="fw-bold">capacity : </span>
+            <span className="ms-2">{capacity}</span>
+          </div>
+          <div className="d-flex p-3">
+            <span className="fw-bold">status : </span>
+            <span className="ms-2">{status}</span>
+          </div>
+        </div>
+        <div className="col-6">
+          <div className="d-flex p-3">
+            <span className="fw-bold">start date : </span>
+            <span className="ms-2">{start_date}</span>
+          </div>
+          <div className="d-flex p-3">
+            <span className="fw-bold">end date : </span>
+            <span className="ms-2">{end_date}</span>
+          </div>
+        </div>
+      </div>
+    </Container>
+  );
+};
+
+export default ShowCourseComp;
--- src/pages/showCourse.page.js
+++ src/pages/showCourse.page.js
@@ -1,14 +1,36 @@
+import { useParams } from "react-router-dom";
+import useSWR from "swr";
+import ShowCourseComp from "../components/courses/showCourse/showCourse.component";
+
 const ShowCourse = () => {
+  //get the id prams
+  const { id } = useParams();
+  //fetch course data by SWR to easy handle fetching and caching data if there isn't any changes to avoid unnessary requests
+  const fetcher = (url) => fetch(url).then((res) => res.json());
+  const {
+    data: courseData,
+    error,
+    isLoading,
+  } = useSWR(
+    `https://sheetdb.io/api/v1/owv6j5nrau720/search?id=${id}`,
+    fetcher
+  );
+
   return (
     <section
       style={{
-        height: "100vh",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
+        minHeight: "100vh",
       }}
     >
-      <h1>ShowCourse</h1>
+      {isLoading ? (
+        <div> Loading...</div>
+      ) : error ? (
+        <div> An error has occurred.</div>
+      ) : courseData[0]?.id ? (
+        <ShowCourseComp courseData={courseData[0]} />
+      ) : (
+        "not found"
+      )}
     </section>
   );
 };
